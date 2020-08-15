3b33428cb9381aab3da4c46bb7da6a11fb42dff9
Tim Reichen <tim.reichen@gmail.com>
2020-08-15 22:16:35 +0200

    add

---
+++ mod.ts
@@ -0,0 +1,95 @@
+import { posix } from "https://deno.land/std@0.65.0/path/mod.ts";
+
+interface SpecifierMap {
+  [specifier: string]: string;
+}
+
+interface Scopes {
+  [url: string]: SpecifierMap;
+}
+
+interface ImportMap {
+  imports?: SpecifierMap;
+  scopes?: Scopes;
+}
+
+function createAsURL(specifier: string, baseURL: string) {
+  try {
+    if (
+      specifier.startsWith("/") || specifier.startsWith("./") ||
+      specifier.startsWith("../")
+    ) {
+      return new URL(specifier, baseURL);
+    } else {
+      return new URL(specifier);
+    }
+  } catch {
+    return null;
+  }
+}
+
+function resolveImportMatch(
+  normalizedSpecifier: string,
+  specifierMap: SpecifierMap,
+) {
+  for (const [specifierKey, resolutionResult] of Object.entries(specifierMap)) {
+    if (resolutionResult === null) {
+      throw Error(`resolution of specifierKey was blocked by a null entry.`);
+    }
+    if (specifierKey === normalizedSpecifier) {
+      return resolutionResult;
+    } else if (
+      specifierKey.endsWith("/") && normalizedSpecifier.startsWith(specifierKey)
+    ) {
+      const afterPrefix = normalizedSpecifier.slice(specifierKey.length);
+      return posix.join(resolutionResult, afterPrefix);
+    }
+  }
+  return null;
+}
+
+function resolveModuleSpecifier(
+  specifier: string,
+  { imports = {}, scopes = {} }: ImportMap,
+  baseURL: string,
+) {
+  const baseURLString = baseURL;
+  const asURL = createAsURL(specifier, baseURL);
+  const normalizedSpecifier = asURL?.toString() || specifier;
+  for (const [scopePrefix, scopeImports] of Object.entries(scopes)) {
+    if (
+      scopePrefix === baseURLString ||
+      (scopePrefix.endsWith("/") && baseURLString.startsWith(scopePrefix))
+    ) {
+      const scopeImportsMatch = resolveImportMatch(
+        normalizedSpecifier,
+        scopeImports,
+      );
+      if (scopeImportsMatch) return scopeImportsMatch;
+    }
+  }
+  const topLevelImportsMatch = resolveImportMatch(normalizedSpecifier, imports);
+  if (topLevelImportsMatch) return topLevelImportsMatch;
+  if (asURL) return asURL;
+  throw Error(
+    `specifier was a bare specifier, but was not remapped to anything by importMap.`,
+  );
+}
+
+/**
+ * resolves specifier with import map.
+ * ```ts
+ * import { resolve } from "https://deno.land/x/importmap/mod.ts"
+ * 
+ * const specifier = "foo/mod.ts"
+ * const importMap = { imports: { "foo/": "bar/" } }
+ * const resolvedSpecifier = resolve(specifier, importMap) // returns "bar/mod.ts"
+ * ```
+ */
+export function resolve(
+  specifier: string,
+  importMap: ImportMap,
+  baseURL = ".",
+) {
+  return resolveModuleSpecifier(specifier, importMap, baseURL);
+}
---
+++ test.ts
@@ -0,0 +1,45 @@
+import { resolve } from "./mod.ts";
+import { assertEquals } from "https://deno.land/std@0.65.0/testing/asserts.ts";
+
+Deno.test("resolve imports", () => {
+  const importMap = {
+    imports: {
+      "foo/": "bar/",
+      "/app/helpers.mjs": "/app/helpers/index.mjs",
+    },
+  };
+  assertEquals(resolve("foo/test", importMap), "bar/test");
+  assertEquals(
+    resolve("/app/helpers.mjs", importMap),
+    "/app/helpers/index.mjs",
+  );
+});
+
+Deno.test("resolve imports with scopes", () => {
+  const importMap = {
+    imports: {
+      "a": "/a-1.mjs",
+      "b": "/b-1.mjs",
+      "c": "/c-1.mjs",
+    },
+    scopes: {
+      "/scope2/": {
+        "a": "/a-2.mjs",
+      },
+      "/scope2/scope3/": {
+        "b": "/b-3.mjs",
+      },
+    },
+  };
+  assertEquals(resolve("a", importMap, "/scope1/foo.mjs"), "/a-1.mjs");
+  assertEquals(resolve("b", importMap, "/scope1/foo.mjs"), "/b-1.mjs");
+  assertEquals(resolve("c", importMap, "/scope1/foo.mjs"), "/c-1.mjs");
+
+  assertEquals(resolve("a", importMap, "/scope2/foo.mjs"), "/a-2.mjs");
+  assertEquals(resolve("b", importMap, "/scope2/foo.mjs"), "/b-1.mjs");
+  assertEquals(resolve("c", importMap, "/scope2/foo.mjs"), "/c-1.mjs");
+
+  assertEquals(resolve("a", importMap, "/scope2/scope3/foo.mjs"), "/a-2.mjs");
+  assertEquals(resolve("b", importMap, "/scope2/scope3/foo.mjs"), "/b-3.mjs");
+  assertEquals(resolve("c", importMap, "/scope2/scope3/foo.mjs"), "/c-1.mjs");
+});
